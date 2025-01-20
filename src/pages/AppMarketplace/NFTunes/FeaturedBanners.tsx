34bde38718cb0309da879d12af8c633093f47e06
Kael Venn <sigmagentic.cloud@gmail.com>
2025-01-20 12:12:52 +1100

    feature: new banner

--- src/pages/AppMarketplace/NFTunes/FeaturedBanners.tsx
+++ src/pages/AppMarketplace/NFTunes/FeaturedBanners.tsx
@@ -15,17 +15,17 @@ export const FeaturedBanners = ({ onFeaturedArtistDeepLinkSlug }: { onFeaturedAr
             id="featured1"
             className="flex md:flex-1 mt-2 min-h-[200px] md:h-[330px] bg-no-repeat bg-cover rounded-3xl w-[100%] border-[1px] border-foreground/20"
             style={{
-              "backgroundImage": `url(https://api.itheumcloud.com/app_nftunes/assets/img/BeatsbyScooby.jpg)`,
+              "backgroundImage": `url(https://api.itheumcloud.com/app_nftunes/assets/img/Uncomplex.jpg)`,
               "backgroundBlendMode": "multiply",
               "backgroundColor": "#161616a3",
             }}>
             <div className="flex flex-col h-[100%] m-auto justify-center items-center">
-              <h1 className="!text-lg !text-white text-center md:!text-2xl mb-5">Scooby's "The Art of Self Destruction" is Live!</h1>
+              <h1 className="!text-lg !text-white text-center md:!text-2xl mb-5">Uncomplex's "ISHQNAMA - JOURNEY OF LOVE" is Live!</h1>
 
               <Button
                 className="!text-black text-sm tracking-tight relative px-[2.35rem] left-2 bottom-1.5 bg-gradient-to-r from-yellow-300 to-orange-500 transition ease-in-out delay-150 duration-300 hover:translate-y-1.5 hover:-translate-x-[8px] hover:scale-100"
                 onClick={() => {
-                  onFeaturedArtistDeepLinkSlug("beats-by-scooby");
+                  onFeaturedArtistDeepLinkSlug("uncomplex");
                   scrollToSection("artist-profile");
                 }}>
                 Collect & Listen
