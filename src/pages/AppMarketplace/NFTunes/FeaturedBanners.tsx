6b013366df4492608b4ccd7adc90ddc02d850bd6
Kael Venn <sigmagentic.cloud@gmail.com>
2025-01-29 10:14:44 +1100

    feature: new featured artist

--- src/pages/AppMarketplace/NFTunes/FeaturedBanners.tsx
+++ src/pages/AppMarketplace/NFTunes/FeaturedBanners.tsx
@@ -15,6 +15,28 @@ export const FeaturedBanners = ({ onFeaturedArtistDeepLinkSlug }: { onFeaturedAr
             id="featured1"
             className="flex md:flex-1 mt-2 min-h-[200px] md:h-[330px] bg-no-repeat bg-cover rounded-3xl w-[100%] border-[1px] border-foreground/20"
             style={{
+              "backgroundImage": `url(https://api.itheumcloud.com/app_nftunes/assets/img/Waveborn_Luminex_Lost_And_Wondering.jpeg)`,
+              "backgroundBlendMode": "multiply",
+              "backgroundColor": "#161616a3",
+            }}>
+            <div className="flex flex-col h-[100%] m-auto justify-center items-center">
+              <h1 className="!text-lg !text-white text-center md:!text-2xl mb-5">Waveborn's "Lost And Wondering" is Live!</h1>
+
+              <Button
+                className="!text-black text-sm tracking-tight relative px-[2.35rem] left-2 bottom-1.5 bg-gradient-to-r from-yellow-300 to-orange-500 transition ease-in-out delay-150 duration-300 hover:translate-y-1.5 hover:-translate-x-[8px] hover:scale-100"
+                onClick={() => {
+                  onFeaturedArtistDeepLinkSlug("waveborn-luminex");
+                  scrollToSection("artist-profile");
+                }}>
+                Collect & Listen
+              </Button>
+            </div>
+          </div>
+
+          <div
+            id="featured2"
+            className="flex md:flex-1 mt-2 min-h-[200px] md:h-[330px] bg-no-repeat bg-cover rounded-3xl w-[100%] border-[1px] border-foreground/20"
+            style={{
               "backgroundImage": `url(https://api.itheumcloud.com/app_nftunes/assets/img/Uncomplex.jpg)`,
               "backgroundBlendMode": "multiply",
               "backgroundColor": "#161616a3",
@@ -26,28 +48,6 @@ export const FeaturedBanners = ({ onFeaturedArtistDeepLinkSlug }: { onFeaturedAr
                 className="!text-black text-sm tracking-tight relative px-[2.35rem] left-2 bottom-1.5 bg-gradient-to-r from-yellow-300 to-orange-500 transition ease-in-out delay-150 duration-300 hover:translate-y-1.5 hover:-translate-x-[8px] hover:scale-100"
                 onClick={() => {
                   onFeaturedArtistDeepLinkSlug("uncomplex");
-                  scrollToSection("artist-profile");
-                }}>
-                Collect & Listen
-              </Button>
-            </div>
-          </div>
-
-          <div
-            id="featured1"
-            className="flex md:flex-1 mt-2 min-h-[200px] md:h-[330px] bg-no-repeat bg-cover rounded-3xl w-[100%] border-[1px] border-foreground/20"
-            style={{
-              "backgroundImage": `url(https://api.itheumcloud.com/app_nftunes/assets/img/Physixdude.jpg)`,
-              "backgroundBlendMode": "multiply",
-              "backgroundColor": "#161616a3",
-            }}>
-            <div className="flex flex-col h-[100%] m-auto justify-center items-center">
-              <h1 className="!text-lg !text-white text-center md:!text-2xl mb-5">Physixdude's "Angles of Identity" is Live!</h1>
-
-              <Button
-                className="!text-black text-sm tracking-tight relative px-[2.35rem] left-2 bottom-1.5 bg-gradient-to-r from-yellow-300 to-orange-500 transition ease-in-out delay-150 duration-300 hover:translate-y-1.5 hover:-translate-x-[8px] hover:scale-100"
-                onClick={() => {
-                  onFeaturedArtistDeepLinkSlug("physixdude");
                   scrollToSection("artist-profile");
                 }}>
                 Collect & Listen
