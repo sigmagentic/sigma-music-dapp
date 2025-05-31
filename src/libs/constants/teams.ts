15c7a2dd28202b8da093fbf5bab182963f8ded36
Kael Venn <sigmagentic.cloud@gmail.com>
2025-05-31 22:28:07 +1000

    chore: new teams to wsb

--- src/libs/constants/teams.ts
+++ src/libs/constants/teams.ts
@@ -4,12 +4,10 @@ export interface Team {
 }
 
 export const TEAMS: Record<string, Team[]> = {
-  phl: [
-    { teamName: "ROCK*WELLPH", teamCode: "mrw" },
-    { teamName: "GBOYZ JUNIORS", teamCode: "gbj" },
-    { teamName: "Teen The Alive One", teamCode: "12" },
-    { teamName: "Monster LCDC", teamCode: "13" },
-    { teamName: "Open AMK Rocknation", teamCode: "15" },
+  nzw: [
+    { teamName: "Rezpect Kidz", teamCode: "rzk" },
+    { teamName: "Rezpect", teamCode: "rzt" },
+    { teamName: "Swagga", teamCode: "swg" },
   ],
   zaf: [
     { teamName: "XO Kiddzz Elite", teamCode: "opn" },
@@ -18,15 +16,18 @@ export const TEAMS: Record<string, Team[]> = {
     { teamName: "CPC", teamCode: "7" },
     { teamName: "Da-Sturbin Da Peace", teamCode: "8" },
   ],
+  phl: [
+    { teamName: "ROCK*WELLPH", teamCode: "mrw" },
+    { teamName: "GBOYZ JUNIORS", teamCode: "gbj" },
+    { teamName: "Teen The Alive One", teamCode: "12" },
+    { teamName: "Monster LCDC", teamCode: "13" },
+    { teamName: "Open AMK Rocknation", teamCode: "15" },
+  ],
   ind: [
     { teamName: "V-Unbeatable", teamCode: "vub" },
     { teamName: "Teen Zero Degree Crew", teamCode: "1" },
     { teamName: "Cell Slumz In Street Crew", teamCode: "2" },
     { teamName: "Open Team One Crew", teamCode: "3" },
-  ],
-  nzw: [
-    { teamName: "Opens Swagga", teamCode: "9" },
-    { teamName: "Cell GFam", teamCode: "10" },
   ],
   sin: [],
   aus: [],
